eabf1ad7c0ce0855e545e81c9bb255b911baa630
dhashr <kalidashramachandran@gmail.com>
2021-09-20 10:40:27 +0530

    Commit Message

---
+++ scripting.js
@@ -0,0 +1,117 @@
+// --------------mainpage--------------
+
+// git redirect page  
+function gitmainpage(){
+    window.location = "githubmain.html"
+}  
+function gitfunction(){
+    window.location = "github.html";
+}
+
+// view on signin
+document.querySelector("#signbtn").addEventListener("click", togglesign)
+function togglesign() {
+    var signup = document.getElementById('signupform');
+    var login = document.getElementById('loginform');
+    if (signup.style.display == 'none') {
+        signup.style.display = 'block';
+        login.style.display = 'none'  
+    }
+}
+
+document.querySelector("#loginbtn").addEventListener("click", togglelogin)
+function togglelogin() {
+    var login = document.getElementById('loginform');
+    var signup = document.getElementById('signupform');
+    if (login.style.display == 'none') {
+        login.style.display = 'block';
+        signup.style.display = 'none'  
+    }
+}
+
+// ---------LoginForm-------------
+document.querySelector("#signup").addEventListener("click", checksigncondition)
+function checksigncondition(){
+    if(document.getElementById("f_name").value == ""){
+        alert("Fill the FirstName");
+        return false
+    }
+    else if(document.getElementById("l_name").value == ""){
+        alert("Fill the LastName");
+        return false
+    }
+    else if(document.getElementById("dob").value == ""){
+        alert("Fill the DOB ");
+        return false
+    }
+    else if(document.getElementById("gender").value == ""){
+        alert("Fill the Gender ");
+        return false
+    }
+    else if(document.getElementById("mail").value == ""){
+        alert("Fill the E-mail ");
+        return false
+    }
+    else if(document.getElementById("pwd").value == ""){
+        alert("Fill the Password ");
+        return false
+    }
+    else{
+        document.querySelector("#signup").addEventListener("click", togglesignup)
+        function togglesignup() {
+            let tonxt = document.getElementById('tonxt');
+            if (tonxt.style.display == 'none') {
+                tonxt.style.display = 'block';  
+            }
+        } 
+    }
+}
+
+document.querySelector("#loginin").addEventListener("click", checklogincondition)
+function checklogincondition(){
+    if(document.getElementById("mail1").value == ""){
+        alert("Fill the E-mail ");
+        return false
+    }
+    else if(document.getElementById("pwd1").value == ""){
+        alert("Fill the Password");
+        return false
+    }
+    else{
+        document.querySelector("#loginin").addEventListener("click", toggleloginin)
+        function toggleloginin() {
+            let tonxt1 = document.getElementById('tonxt1');
+            if (tonxt1.style.display == 'none') {
+                tonxt1.style.display = 'block';  
+            }
+        } 
+    }
+}
+    
+  
+// -------------localStorage-------------
+function storesigndata(){
+    let f_name = document.querySelector("#f_name").value;
+    let l_name = document.querySelector("#l_name").value;
+    let dob = document.querySelector("#dob").value;
+    let gender = document.querySelector("#gender").value;
+    let email = document.querySelector("#mail").value;
+    let pwd = document.querySelector("#pwd").value;
+    
+    localStorage.setItem("Sign up firstname",f_name)
+    localStorage.setItem("Sign up lastname",l_name)
+    localStorage.setItem("Sign up dob",dob)
+    localStorage.setItem("Sign up gender",gender)
+    localStorage.setItem("Sign up email",email)
+    localStorage.setItem("Sign up password",pwd)
+}
+
+function storelogindata(){
+    let email1 = document.querySelector("#mail1").value;
+    let pwd1 = document.querySelector("#pwd1").value;
+    
+    localStorage.setItem("Login Email",email1)
+    localStorage.setItem("Login password",pwd1)
+}
+
+
---
+++ scriptinggit.js
@@ -0,0 +1,57 @@
+// -----------date & time-----------
+
+let live_date = new Date();
+let monthname = ["JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEPT", "OCT", "NOV", "DEC"]
+let month = monthname[live_date.getMonth()];
+let date = live_date.getDate();
+let year = live_date.getFullYear();
+let hour = live_date.getHours();
+let min = live_date.getMinutes();
+document.querySelector(".livedate").innerHTML = date + "/" + month + "/" + year;
+document.querySelector(".livetime").innerHTML = hour + ":" + min;
+
+
+// ---------------github------------------
+
+let main = document.querySelector("#main")
+document.getElementById("gsearch").addEventListener("click",gfunction)
+function gfunction(){
+    let ginput = document.querySelector("#ginput").value;
+    let xhrrequest = new XMLHttpRequest();
+    xhrrequest.open("GET", `https://api.github.com/users/${ginput}`);
+    xhrrequest.onreadystatechange = function (){
+        if(this.readyState === XMLHttpRequest.DONE && this.status === 200){
+            let response = JSON.parse(this.response);
+            console.log(response);
+            let img = document.createElement("img");
+            img.setAttribute("id", "gprofile");
+            main.appendChild(img);
+            let p1 = document.createElement("p1");
+            p1.setAttribute("id", "gpname");
+            main.appendChild(p1);
+            let p2 = document.createElement("p2");
+            p2.setAttribute("id", "gname");
+            main.appendChild(p2);
+            let p3 = document.createElement("p3");
+            p3.setAttribute("id", "gbio");
+            main.appendChild(p3);
+            let p4 = document.createElement("p4");
+            p4.setAttribute("id", "grepos");
+            main.appendChild(p4);
+            let p5 = document.createElement("p5");
+            p5.setAttribute("id", "gemail");
+            main.appendChild(p5);
+            let p6 = document.createElement("p6");
+            p6.setAttribute("id", "glocation");
+            main.appendChild(p6);
+            img.src = `${response.avatar_url}`; 
+            p1.innerHTML = `Profile Name :${response.login}`; 
+            p2.innerHTML = `Name :${response.name}`; 
+            p3.innerHTML = `Bio :${response.bio}`;
+            p4.innerHTML = `Repositories :${response.public_repos}`;
+            p5.innerHTML = `E-Mail :${response.email}`;
+            p6.innerHTML = `Location :${response.location}`;
+        }
+    }
+    xhrrequest.send();
+} 
